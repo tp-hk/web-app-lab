5d66b3f53c873d5d1b2f550b280604715deca25f
Tif-P-HK <bads_turtleboard@hotmail.com>
2015-12-26 15:22:07 -0800

    - code reorg - initial commit of todo-mvc app

---
+++ EmberLab/emberCli/todo-mvc/app/mirage/config.js
@@ -0,0 +1,35 @@
+export default function() {
+  this.get('/todos', function(db, request) {
+    return {
+      data: db.todos.map(attrs => (
+      {type: 'todos', id: attrs.id, attributes: attrs }
+      ))
+    };
+  });
+
+  this.post('/todos', function(db, request) {
+    let attrs = JSON.parse(request.requestBody);
+    let todo = db.todos.insert(attrs);
+    return {
+      data: {
+        type: 'todos',
+        id: todo.id,
+        attributs: todo
+      }
+    };
+  });
+
+  this.patch('/todos/:id', function(db, request) {
+    let attrs = JSON.parse(request.requestBody);
+    let todo = db.todos.update(attrs.data.id, attrs.data.attributes);
+    return {
+      data: {
+        type: "todos",
+        id: todo.id,
+        attributes: todo
+      }
+    };
+  });
+
+  this.del('/todos/:id');
+}
